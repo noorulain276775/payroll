00e26bf557b191b07b9ffb5f648e26492d19b50b
Noor <noorulainibrahim75@gmail.com>
2025-05-21 09:59:47 +0400

    Testing CI/CD Pipelines

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -6,7 +6,7 @@ const AppFooter = () => {
     <CFooter className="px-4">
       <div>
         <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          Liya Trading
+          WorkSphere
         </a>
         <span className="ms-1">&copy; 2025</span>
       </div>
